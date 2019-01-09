71ceb4bc7b023e03f12fefeee5cd478aef4af1fb
jdljf <1243270526@qq.com>
2019-01-09 15:16:48 +0800

    改变数据接口，重写消息详情页面的接口、添加了添加评论接口

--- models/myMessage.js
+++ models/myMessage.js
@@ -4,30 +4,33 @@ mongoose.connect('mongodb://localhost/newTreate', { useNewUrlParser: true })
 
 const db = mongoose.connection
 
+const messageDetailSchema = mongoose.Schema({
+    detail: String,
+    create_Time: {
+        type: Date
+    }
+})
+
+const messageSchema = mongoose.Schema({
+    watched: Boolean,
+    messageType: Number,
+    summary: String,
+    title: String,
+    detail: Array,
+    create_Time: {
+        type: Date,
+        default: Date.now
+    },
+    messageDetail: [messageDetailSchema]
+})
+
 const myMessageSchema = mongoose.Schema({
     personId: String,
     create_Time: {
         type: Date,
         default: Date.now
     },
-    message: {
-        type: Array,
-        watched: Boolean,
-        messageType: Number,
-        summary: String,
-        title: String,
-        detail: Array,
-        create_Time: {
-            type: Date,
-            default: Date.now
-        },
-        messageDetail: {
-            type: Array,
-            index: { type: Number, index: true },
-            detail: String,
-            create_Time: String
-        }
-    }
+    message: [messageSchema]
 })
 
 module.exports = mongoose.model('Mymessage', myMessageSchema);
